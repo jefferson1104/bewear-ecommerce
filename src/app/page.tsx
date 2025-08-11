538a352fc09f8020cac0c5392a184d47f95d399b
Jefferson Soares <48356215+jefferson1104@users.noreply.github.com>
2025-08-10 22:41:50 -0300

    feat: add footer components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
+import { desc } from "drizzle-orm";
 import Image from "next/image";
 
 import { Header } from "@/components//header";
 import { ProductList } from "@/components//product-list";
 import { CategorySelector } from "@/components/category-selector";
+import { Footer } from "@/components/footer";
 import { db } from "@/db";
+import { productsTable } from "@/db/schema";
 
 export default async function HomePage() {
   // Constants
   const categories = await db.query.categoriesTable.findMany();
+
   const products = await db.query.productsTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+
+  const newlyCreatedProducts = await db.query.productsTable.findMany({
+    orderBy: [desc(productsTable.createdAt)],
     with: {
       variants: true,
     },
@@ -16,38 +27,40 @@ export default async function HomePage() {
 
   // Renders
   return (
-    <>
+    <main className="space-y-6">
       <Header />
 
-      <div className="space-y-6">
-        <div className="px-5">
-          <Image
-            src="/images/home-banner-01.png"
-            alt="Leve uma vida com estilo"
-            height={0}
-            width={0}
-            sizes="100vw"
-            className="h-auto w-full"
-          />
-        </div>
-
-        <ProductList title="Mais vendidos" products={products} />
-
-        <div className="px-5">
-          <CategorySelector categories={categories} />
-        </div>
-
-        <div className="px-5">
-          <Image
-            src="/images/home-banner-02.png"
-            alt="Seja autêntico"
-            height={0}
-            width={0}
-            sizes="100vw"
-            className="h-auto w-full"
-          />
-        </div>
+      <div className="px-5">
+        <Image
+          src="/images/home-banner-01.png"
+          alt="Leve uma vida com estilo"
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="h-auto w-full"
+        />
       </div>
-    </>
+
+      <ProductList title="Best sellers" products={products} />
+
+      <div className="px-5">
+        <CategorySelector categories={categories} />
+      </div>
+
+      <div className="px-5">
+        <Image
+          src="/images/home-banner-02.png"
+          alt="Seja autêntico"
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="h-auto w-full"
+        />
+      </div>
+
+      <ProductList title="New arrivals" products={newlyCreatedProducts} />
+
+      <Footer />
+    </main>
   );
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+export function Footer() {
+  // Renders
+  return (
+    <div className="bg-accent w-full gap-1 p-8">
+      <p className="text-xs font-medium">@ 2025 Copyright SOARES DEV LTDA.</p>
+      <p className="text-muted-foreground text-xs font-medium">
+        All rights reserved.
+      </p>
+    </div>
+  );
+}
