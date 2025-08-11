97931ef5c8830df01b7c4b91edd5863781b2cf7f
Jefferson Soares <48356215+jefferson1104@users.noreply.github.com>
2025-08-10 23:47:00 -0300

    feat: add social sign in with google

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -19,6 +19,8 @@ export function Header() {
   // Constants
   const { data: session } = authClient.useSession();
 
+  console.log("session", session);
+
   // Renders
   return (
     <header className="flex items-center justify-between p-5">
